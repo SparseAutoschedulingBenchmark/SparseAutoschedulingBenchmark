60e4de50e753a46a3f1eeab6458e1335b2e39142
Dinofish32 <akarshd32@gmail.com>
2025-12-05 17:47:42 -0500

    rewrote forward_euler to use benchmark function format

--- src/sparseappbench/benchmarks/circuitsim.py
+++ src/sparseappbench/benchmarks/circuitsim.py
@@ -5,32 +5,39 @@ Email: aduddu3@gatech.edu
 """
 
 from collections.abc import Callable
-from functools import partial
-
 import numpy as np
 
+from functools import partial
 import matplotlib.pyplot as plt
 
 
 def forward_euler(
-    dydx: Callable[[float, float], float], span: tuple[float, float], y0: np.ndarray,
-    first_step: float | None = None, **kwargs
-) -> tuple[np.ndarray, np.ndarray]:
+    xp,
+    dydx: Callable[[float, float], float],
+    span: tuple[float, float],
+    y0: np.ndarray,
+    first_step: float,
+):
     """Forward Euler method of approximating ordinary differential equations (ODEs)."""
-    if first_step is None:
-        inputs = np.linspace(span[0], span[1], 1000)
-        step = inputs[1] - inputs[0]
-    else:
-        inputs = np.arange(span[0], span[1], first_step)
-        step = first_step
-    outputs = np.zeros((len(inputs), len(y0)))
+    # Builtin range function does not support floating-point step
+    curr = span[0]
+    inputs = []
+    while curr < span[1]:
+        inputs.append(curr)
+        curr += first_step
+
+    step = first_step
+    outputs = [None for _ in inputs]
     outputs[0] = y0
 
-    for i in range(1, inputs.shape[0]):
+    for i in range(1, len(inputs)):
         # y_new = y + dy/dx * delta x
-        outputs[i] = (
-            outputs[i - 1] + np.array(dydx(inputs[i - 1], outputs[i - 1])) * step
-        )
+
+        dydt_vector = dydx(inputs[i - 1], outputs[i - 1])
+        outputs[i] = [
+            outputs[i - 1][j] + dydt_vector[j] * step
+            for j in range(len(y0))
+        ]
 
     return (inputs, outputs)
 
@@ -38,7 +45,7 @@ def forward_euler(
 def rc(t: float, Vc: float, R: float, C: float, Vs_func: Callable[[float], float]) -> float:
     tau = R * C
     Vs = Vs_func(t)  # Get the current source voltage (allows for any input!)
-    return (Vs - Vc) / tau # dV/dt
+    return [(Vs - Vc[0]) / tau] # dV/dt
 
 
 def rlc(t: float, state: np.ndarray, R: float, L: float, C: float, Vs_func: Callable[[float], float]) -> tuple[float, float]:
@@ -72,10 +79,10 @@ def display_rc() -> None:
     dVdt = partial(rc, R=R, C=C, Vs_func=step_input)
     # dVdt = lambda t, Vc: rc_ode(t, Vc, R, C, step_input)
 
-    time, voltage = forward_euler(dVdt, (0, t_max), [V_C_initial])
+    time, voltage = forward_euler(np, dVdt, (0, t_max), [V_C_initial], t_max / 1000)
 
     plt.figure(figsize=(8, 5))
-    plt.plot(time * 1e3, voltage, label="Numerical Solution")
+    plt.plot([t * 1e3 for t in time], voltage, label="Numerical Solution")
     plt.title("RC Circuit Charging - Numerical Solution")
     plt.xlabel("Time (ms)")
     plt.ylabel("Capacitor Voltage (V)")
@@ -91,16 +98,15 @@ def display_lv() -> None:
     c = 0.4  # predator death rate
     d = 0.1  # predator reproduction rate
 
-    y0 = np.ndarray([10, 5]) # Initial prey, initial predators
+    y0 = np.array([10, 5]) # Initial prey, initial predators
 
     dydt = partial(lotka_volterra, a=a, b=b, c=c, d=d)
     # dydt = lambda t, y: lotka_volterra(t, y, a, b, c, d)
 
     # Solve
-    t, sol = forward_euler(dydt, (0, 30), y0)
-    prey = sol[:, 0]
-    predators = sol[:, 1]
-
+    t, sol = forward_euler(np, dydt, (0, 30), y0, 1)
+    prey, predators = zip(*sol)
+    
     plt.figure(figsize=(10, 6))
     plt.plot(t, prey, label="Prey")
     plt.plot(t, predators, label="Predators")
@@ -121,9 +127,9 @@ def display_rlc() -> None:
     y0 = np.array([0.0, 0.0]) # y0[0] is Vc, y0[1] is dVc/dt
     dVdt = partial(rlc, R=R, L=L, C=C, Vs_func=step_input)
     # dVdt = lambda t, y: rlc(t, y, R, L, C, step_input)
-    t, sol = forward_euler(dVdt, (0, t_max), y0)
+    t, sol = forward_euler(np, dVdt, (0, t_max), y0, t_max/1000)
 
-    plt.plot(t * 1000, sol[:, 0])
+    plt.plot([i * 1000 for i in t], [i[0] for i in sol])
     plt.xlabel("Time (ms)")
     plt.ylabel("Capacitor Voltage (V)")
     plt.title("Series RLC Step Response (Using solve_ivp2)")
@@ -133,4 +139,4 @@ def display_rlc() -> None:
 
 
 if __name__ == "__main__":
-    display_rc()
+    display_lv()
